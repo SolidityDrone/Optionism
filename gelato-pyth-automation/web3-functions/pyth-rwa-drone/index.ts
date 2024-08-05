ad4b45859d4f9e6999bf03aaeec32e55ef302b0a
SolidityDrone <pnn.claudoio@gmail.com>
2024-08-05 05:35:05 +0200

    lastfeed->updateData

--- gelato-pyth-automation/web3-functions/pyth-rwa-drone/index.ts
+++ gelato-pyth-automation/web3-functions/pyth-rwa-drone/index.ts
@@ -5,10 +5,9 @@ import {
 import { Contract, utils, BigNumber, BytesLike } from "ethers";
 import { EvmPriceServiceConnection } from "@pythnetwork/pyth-evm-js";
 import { optionismAbi } from "./optionismAbi";
-interface IPRICE {
-  price: number;
-  timestamp: number;
-}
+
+
+
 Web3Function.onRun(async (context: Web3FunctionContext) => {
   const { userArgs, storage, secrets, multiChainProvider } = context;
 
@@ -21,122 +20,123 @@ Web3Function.onRun(async (context: Web3FunctionContext) => {
     optionismAbi,
     provider
   );
-  
-  // Retrieve and parse the awaiting value from storage
-const storageAwaitingValue = await storage.get("storageAwaiting");
-const parsedAwaitingArray = storageAwaitingValue ? storageAwaitingValue.split(",").map(Number) : [];
+    
+    // Retrieve and parse the awaiting value from storage
+  const storageAwaitingValue = await storage.get("awaiting");
+  const parsedAwaitingArray = storageAwaitingValue ? storageAwaitingValue.split(",").map(Number) : [];
 
-// Call getArrayChunk function on the contract
-const chunk = await optionismContract.getArrayChunk(0, 2);
+  // Call getArrayChunk function on the contract
+  const chunk = await optionismContract.getArrayChunk(0, 10);
 
-// Destructure the response to get the arrays
-const [retrievedOptionIds, retrievedExpiries, retrievedPriceIds] = chunk;
+  // Destructure the response to get the arrays
+  const [retrievedOptionIds, retrievedExpiries, retrievedPriceIds] = chunk;
 
-const chunkExpiriesArray = retrievedExpiries.map((expiry: BigNumber) => expiry.toNumber());
-const chunkPriceIdsArray = retrievedPriceIds.map((id: BytesLike) => utils.hexlify(id));
-const chunkOptionIdsArray = retrievedOptionIds.map((opId: BigNumber) => opId.toNumber());
+  const chunkExpiriesArray = retrievedExpiries.map((expiry: BigNumber) => expiry.toNumber());
+  const chunkPriceIdsArray = retrievedPriceIds.map((id: BytesLike) => utils.hexlify(id));
+  const chunkOptionIdsArray = retrievedOptionIds.map((opId: BigNumber) => opId.toNumber());
 
-console.log("Total Items:", chunkExpiriesArray.length);
+  console.log("Total Items:", chunkExpiriesArray.length);
 
-// Get the current Unix timestamp in seconds
-const currentUnixTimestamp = Math.floor(Date.now() / 1000);
+  // Get the current Unix timestamp in seconds
+  const currentUnixTimestamp = Math.floor(Date.now() / 1000);
 
-// Filter out the items with expiry less than or equal to the current timestamp
-const validExpiriesArray = chunkExpiriesArray.filter((expiry: number) => expiry <= currentUnixTimestamp);
-let validOptionIdsArray = chunkOptionIdsArray.filter(
-  (_: any, index: number) => chunkExpiriesArray[index] <= currentUnixTimestamp
-);
-const validPriceIdsArray = chunkPriceIdsArray.filter(
-  (_: any, index: number) => chunkExpiriesArray[index] <= currentUnixTimestamp
-);
-
-console.log("Expired Items Count:", validExpiriesArray.length);
-
-// Filter out option IDs that are already in the awaiting array
-validOptionIdsArray = validOptionIdsArray.filter((optionId: number) => !parsedAwaitingArray.includes(optionId));
-
-// Update the awaiting array in storage by removing matched IDs
-const updatedAwaitingArray = parsedAwaitingArray.filter((id: number) => !validOptionIdsArray.includes(id));
-const newStorageAwaitingValue = updatedAwaitingArray.length > 0 ? updatedAwaitingArray.join(",") : "";
-
-// Save the updated awaiting value back to storage
-await storage.set("storageAwaiting", newStorageAwaitingValue);
-
-// Logging for debugging
-console.log("Updated Awaiting value:", newStorageAwaitingValue);
-console.log("Filtered Option IDs for processing:", validOptionIdsArray);
-  // Initialize connection to Pyth
-  const connection = new EvmPriceServiceConnection(
-    "https://hermes-beta.pyth.network"
+  // Filter out the items with expiry less than or equal to the current timestamp
+  const validExpiriesArray = chunkExpiriesArray.filter((expiry: number) => expiry <= currentUnixTimestamp);
+  let validOptionIdsArray = chunkOptionIdsArray.filter(
+    (_: any, index: number) => chunkExpiriesArray[index] <= currentUnixTimestamp
   );
-  const mockPriceId = "0xca80ba6dc32e08d06f1aa886011eed1d77c77be9eb761cc10d72b7d0a2fd57a6";
-  const mockArray = new Array(validPriceIdsArray.length).fill(mockPriceId);
-  // Fetch the latest prices for expired priceIds
-  const priceFeeds = (await connection.getLatestPriceFeeds(chunkPriceIdsArray)) as any[];
+  const validPriceIdsArray = chunkPriceIdsArray.filter(
+    (_: any, index: number) => chunkExpiriesArray[index] <= currentUnixTimestamp
+  );
+
+  console.log("Expired Items Count:", validExpiriesArray.length);
+
+  // Filter out option IDs that are already in the awaiting array
+  validOptionIdsArray = validOptionIdsArray.filter((optionId: number) => !parsedAwaitingArray.includes(optionId));
+
+  // Update the awaiting array in storage by removing matched IDs
+  const updatedAwaitingArray = parsedAwaitingArray.filter((id: number) => !validOptionIdsArray.includes(id));
+  const newStorageAwaitingValue = updatedAwaitingArray.length > 0 ? updatedAwaitingArray.join(",") : "";
+
+  // Save the updated awaiting value back to storage
+  await storage.set("awaiting", newStorageAwaitingValue);
+
+  // Logging for debugging
+  console.log("Updated Awaiting value:", newStorageAwaitingValue);
+  console.log("Filtered Option IDs for processing:", validOptionIdsArray);
+    // Initialize connection to Pyth
+    const connection = new EvmPriceServiceConnection(
+      "https://hermes-beta.pyth.network"
+    );
+    // const mockPriceId = "0xca80ba6dc32e08d06f1aa886011eed1d77c77be9eb761cc10d72b7d0a2fd57a6";
+    // const mockArray = new Array(validPriceIdsArray.length).fill(mockPriceId);
+    
+    // Fetch the latest prices for expired priceIds
+    const priceFeeds = (await connection.getPriceFeedsUpdateData(validPriceIdsArray));
 
 
 
-  
-// Step 1: Parse the awaiting value from storage
-const existingAwaitingValue = await storage.get("awaiting");
-const awaitingArray = existingAwaitingValue ? existingAwaitingValue.split(",").map(Number) : [];
+    
+  // Step 1: Parse the awaiting value from storage
+  const existingAwaitingValue = await storage.get("awaiting");
+  const awaitingArray = existingAwaitingValue ? existingAwaitingValue.split(",").map(Number) : [];
 
-// Step 2: Get the first 10 items in validOptionIdsArray and decode them
-const firstTenExpiredOptionIds = validOptionIdsArray.slice(0, 10).map((id: number) => id.toString());
+  // Step 2: Get the first 10 items in validOptionIdsArray and decode them
+  const firstTenExpiredOptionIds = validOptionIdsArray.slice(0, 10).map((id: number) => id.toString());
 
-// Convert the decoded array to a comma-separated string
-const newAwaitingValue = firstTenExpiredOptionIds.join(",");
+  // Convert the decoded array to a comma-separated string
+  const newAwaitingValue = firstTenExpiredOptionIds.join(",");
 
-// Determine the updated awaiting value
-let updatedAwaitingValue;
+  // Determine the updated awaiting value
+  let updatedAwaitingValue;
 
-// If there is an existing value and new value, concatenate with a comma
-if (existingAwaitingValue?.length != 0 && newAwaitingValue) {
-updatedAwaitingValue = `${existingAwaitingValue},${newAwaitingValue}`;
+  // If there is an existing value and new value, concatenate with a comma
+  if (existingAwaitingValue?.length != 0 && newAwaitingValue) {
+  updatedAwaitingValue = `${existingAwaitingValue},${newAwaitingValue}`;
 
-// If only a new value exists, just use that
-} else if (newAwaitingValue) {
-updatedAwaitingValue = newAwaitingValue;
+  // If only a new value exists, just use that
+  } else if (newAwaitingValue) {
+  updatedAwaitingValue = newAwaitingValue;
 
-// If only existing value exists (new value is empty), use the existing one
-} else {
-updatedAwaitingValue = existingAwaitingValue || "";  // Fallback to empty string if no existing value
-}
+  // If only existing value exists (new value is empty), use the existing one
+  } else {
+  updatedAwaitingValue = existingAwaitingValue || "";  // Fallback to empty string if no existing value
+  }
 
-// Save the updated awaiting value to storage
-await storage.set("awaiting", updatedAwaitingValue);
+  // Save the updated awaiting value to storage
+  await storage.set("awaiting", updatedAwaitingValue);
 
-// Logging for debugging
-console.log("Updated Awaiting value:", updatedAwaitingValue);
+  // Logging for debugging
+  console.log("Updated Awaiting value:", updatedAwaitingValue);
 
-// Step 3: Filter out optionIds that are already in the awaiting array
-const optionIdsToEncode = validOptionIdsArray.filter(
-(optionId: number) => !awaitingArray.includes(optionId)
-).slice(0, 10); // Limit to 10 items
+  // Step 3: Filter out optionIds that are already in the awaiting array
+  const optionIdsToEncode = validOptionIdsArray.filter(
+  (optionId: number) => !awaitingArray.includes(optionId)
+  ).slice(0, 10); // Limit to 10 items
 
-// Encode the optionId, priceId, and priceResult
-const encodedItems = optionIdsToEncode.map((optionId: number, index: number) => {
-const currentPrice: IPRICE = {
-  price: +BigNumber.from(priceFeeds[0].price.price),
-  timestamp: +BigNumber.from(priceFeeds[0].price.publishTime),
-};
+  // Encode the optionId, priceId, and priceResult
+  const encodedItems = optionIdsToEncode.map((optionId: number, index: number) => {
 
-// Encode optionId, priceId, and priceResult
-return utils.defaultAbiCoder.encode(
-  ["uint256", "bytes32", "uint256"],  // uint256 for priceResult
-  [optionId, validPriceIdsArray[index], currentPrice.price]
-);
-});
 
-// Logging for debugging
-console.log("Encoded Items Count:", encodedItems.length);
+  // Encode optionId, priceId, and priceResult
+  return utils.defaultAbiCoder.encode(
+    ["uint256"],  // uint256 for priceResult
+    [optionId]
+  );
+  });
 
-// Prepare the calldata for the contract function
-const iface = new utils.Interface([
-"function gelatoCallBack(bytes[] memory gelato) external",
-]);
+  // Logging for debugging
+  console.log("Encoded Items Count:", encodedItems);
 
-const callData = iface.encodeFunctionData("gelatoCallBack", [encodedItems]);
+  // Prepare the calldata for the contract function
+  const iface = new utils.Interface([
+  "function gelatoCallBack(uint[] memory optionIds, bytes[] memory pythUpdate) external",
+  ]);
+
+  const callData = iface.encodeFunctionData("gelatoCallBack", [
+    optionIdsToEncode,
+    priceFeeds 
+  ]);
 
   if (encodedItems.length != 0) {
     return {
--- gelato-pyth-automation/web3-functions/pyth-rwa-drone/optionismAbi.ts
+++ gelato-pyth-automation/web3-functions/pyth-rwa-drone/optionismAbi.ts
@@ -1,14 +1,539 @@
-export const optionismAbi =[
+export const optionismAbi = [
 	{
-		"inputs": [],
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_gelatoAddress",
+				"type": "address"
+			}
+		],
 		"stateMutability": "nonpayable",
 		"type": "constructor"
 	},
 	{
 		"inputs": [
 			{
+				"internalType": "address",
+				"name": "sender",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "balance",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "needed",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			}
+		],
+		"name": "ERC1155InsufficientBalance",
+		"type": "error"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "approver",
+				"type": "address"
+			}
+		],
+		"name": "ERC1155InvalidApprover",
+		"type": "error"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "idsLength",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "valuesLength",
+				"type": "uint256"
+			}
+		],
+		"name": "ERC1155InvalidArrayLength",
+		"type": "error"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "operator",
+				"type": "address"
+			}
+		],
+		"name": "ERC1155InvalidOperator",
+		"type": "error"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "receiver",
+				"type": "address"
+			}
+		],
+		"name": "ERC1155InvalidReceiver",
+		"type": "error"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "sender",
+				"type": "address"
+			}
+		],
+		"name": "ERC1155InvalidSender",
+		"type": "error"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "operator",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			}
+		],
+		"name": "ERC1155MissingApprovalForAll",
+		"type": "error"
+	},
+	{
+		"inputs": [],
+		"name": "NotGelato",
+		"type": "error"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "account",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "operator",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "bool",
+				"name": "approved",
+				"type": "bool"
+			}
+		],
+		"name": "ApprovalForAll",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "buyer",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "optionId",
+				"type": "uint256"
+			}
+		],
+		"name": "OptionClaim",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "writer",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "optionId",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "optionExpiry",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "premiumUsdcPrice",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "strikePrice",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "buyExpiry",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "shares",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "maximumPayoutPerShare",
+				"type": "uint256"
+			}
+		],
+		"name": "OptionCreated",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "optionId",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "finalPrice",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "bool",
+				"name": "hasToPay",
+				"type": "bool"
+			}
+		],
+		"name": "OptionResolved",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "buyer",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "optionId",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "purchasedShares",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "totalPaid",
+				"type": "uint256"
+			}
+		],
+		"name": "OptionSubscribed",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "operator",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256[]",
+				"name": "ids",
+				"type": "uint256[]"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256[]",
+				"name": "values",
+				"type": "uint256[]"
+			}
+		],
+		"name": "TransferBatch",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "operator",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "id",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "TransferSingle",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "value",
+				"type": "string"
+			},
+			{
+				"indexed": true,
+				"internalType": "uint256",
+				"name": "id",
+				"type": "uint256"
+			}
+		],
+		"name": "URI",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "account",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "id",
+				"type": "uint256"
+			}
+		],
+		"name": "balanceOf",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address[]",
+				"name": "accounts",
+				"type": "address[]"
+			},
+			{
+				"internalType": "uint256[]",
+				"name": "ids",
+				"type": "uint256[]"
+			}
+		],
+		"name": "balanceOfBatch",
+		"outputs": [
+			{
+				"internalType": "uint256[]",
+				"name": "",
+				"type": "uint256[]"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "id",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "amountOfShares",
+				"type": "uint256"
+			}
+		],
+		"name": "buyOption",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "id",
+				"type": "uint256"
+			}
+		],
+		"name": "claimOptionWin",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "bool",
+				"name": "isCallOption",
+				"type": "bool"
+			},
+			{
+				"internalType": "uint256",
+				"name": "premiumUsdcPrice",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "strikePrice",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "buyExpiry",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "optionExpiry",
+				"type": "uint256"
+			},
+			{
+				"internalType": "bytes32",
+				"name": "assetID",
+				"type": "bytes32"
+			},
+			{
+				"internalType": "uint256",
+				"name": "shares",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "maximumPayoutPerShare",
+				"type": "uint256"
+			}
+		],
+		"name": "createOption",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "id",
+				"type": "uint256"
+			}
+		],
+		"name": "deleteOption",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "exhaustedArrays",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256[]",
+				"name": "optionIds",
+				"type": "uint256[]"
+			},
+			{
 				"internalType": "bytes[]",
-				"name": "gelato",
+				"name": "pythUpdate",
 				"type": "bytes[]"
 			}
 		],
@@ -46,6 +571,30 @@ export const optionismAbi =[
 				"internalType": "bytes32[]",
 				"name": "priceIds",
 				"type": "bytes32[]"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "account",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "operator",
+				"type": "address"
+			}
+		],
+		"name": "isApprovedForAll",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
 			}
 		],
 		"stateMutability": "view",
@@ -102,11 +651,6 @@ export const optionismAbi =[
 				"type": "uint256"
 			},
 			{
-				"internalType": "uint256",
-				"name": "sharesPrice",
-				"type": "uint256"
-			},
-			{
 				"internalType": "bytes32",
 				"name": "assetID",
 				"type": "bytes32"
@@ -119,6 +663,11 @@ export const optionismAbi =[
 			{
 				"internalType": "bool",
 				"name": "isActive",
+				"type": "bool"
+			},
+			{
+				"internalType": "bool",
+				"name": "hasToPay",
 				"type": "bool"
 			}
 		],
@@ -145,10 +694,157 @@ export const optionismAbi =[
 		"type": "function"
 	},
 	{
-		"inputs": [],
-		"name": "p",
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "results",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256[]",
+				"name": "ids",
+				"type": "uint256[]"
+			},
+			{
+				"internalType": "uint256[]",
+				"name": "values",
+				"type": "uint256[]"
+			},
+			{
+				"internalType": "bytes",
+				"name": "data",
+				"type": "bytes"
+			}
+		],
+		"name": "safeBatchTransferFrom",
 		"outputs": [],
 		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "id",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "value",
+				"type": "uint256"
+			},
+			{
+				"internalType": "bytes",
+				"name": "data",
+				"type": "bytes"
+			}
+		],
+		"name": "safeTransferFrom",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "operator",
+				"type": "address"
+			},
+			{
+				"internalType": "bool",
+				"name": "approved",
+				"type": "bool"
+			}
+		],
+		"name": "setApprovalForAll",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "bytes4",
+				"name": "interfaceId",
+				"type": "bytes4"
+			}
+		],
+		"name": "supportsInterface",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "uri",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "usdcAddress",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
 		"type": "function"
 	}
 ];
